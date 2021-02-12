e3e0b2adeb818aa8f33bc40f1d96ae7e431aa611
Exinferis <amk@tcs.de>
2021-02-12 19:12:24 +0100

    Minor typings & type safety added.

--- ui/scripts/ui.ts
+++ ui/scripts/ui.ts
@@ -12,9 +12,9 @@ const stylesScene = require('../styles/scene.css');
 export class Ui {
     // We will type those as any for now, just to be able to start from here with typescript
     // TODO: implement interfaces
-    private inputHandler: any;
+    private inputHandler?: InputHandler;
     private controlsSelector: any;
-    private sceneSelector : any;
+    private sceneSelector : HTMLElement | null;
     private detailsSelector: any;
     private controlsDomElement: any;
     private detailsDomElement: any;
@@ -22,26 +22,29 @@ export class Ui {
     private scene: any;
     private controls: any;
 
+    
     /**
-     *
+     * Creates an instance of Ui. The entry point of the whole game ui
      * @param {Document} domDocument
      * @param {String} controlsSelector
-     * @param {String} sceneSelector
-     * @param {String} detailsSelector
+     * @param {*} sceneSelector
+     * @param {*} detailsSelector
+     * @memberof Ui
      */
-    constructor(domDocument, controlsSelector, sceneSelector, detailsSelector) {
-        this.inputHandler = null;
+    constructor(domDocument: Document, controlsSelector: String, sceneSelector: any, detailsSelector: any) {
+        this.inputHandler = undefined;
 
         this.controlsSelector = controlsSelector;
-        this.sceneSelector = sceneSelector;
+        this.sceneSelector = domDocument.querySelector<HTMLElement>(sceneSelector);
         this.detailsSelector = detailsSelector;
 
         this.controlsDomElement = domDocument.querySelector(this.controlsSelector);
-        this.sceneSelector = domDocument.querySelector(this.sceneSelector);
         this.detailsDomElement = domDocument.querySelector(this.detailsSelector);
 
         this.details = new UiDetails(this.detailsDomElement);
-        this.scene = new UiScene(this, this.sceneSelector, domDocument, this.details);
+        // sceneSelector might be undefined, this we will enforce it for now
+        // TODO: Needs to be reworked - prone to fail at any time.
+        this.scene = new UiScene(this, this.sceneSelector!, domDocument, this.details);
         this.controls = new UiControls(this, this.controlsDomElement, this.scene);
     }
 
@@ -60,7 +63,7 @@ export class Ui {
      * @returns {any}
      */
     handleInput(command, data) {
-        return this.inputHandler.command(command, data);
+        return this.inputHandler?.command(command, data);
     }
 
     /**
